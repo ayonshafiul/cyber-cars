f5f88295a0ed65dedddc36bea43154194cecee3e
Ayon <ayonshafiul@users.noreply.github.com>
2020-12-14 16:21:50 +0600

    Add login and index pages

--- server.js
+++ server.js
@@ -3,8 +3,19 @@ const mysql = require("mysql");
 const dotenv = require("dotenv");
 dotenv.config();
 const db = require("./db");
+const path = require("path");
+const ejs = require("ejs");
 
 const app = express();
+const rootDirectoryPath = path.join(__dirname, "public");
+
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(express.static(rootDirectoryPath));
+
+
+app.set("view engine", "ejs");
 
 db.connect((err) => {
     if (err) {
@@ -12,11 +23,28 @@ db.connect((err) => {
     } else {
         console.log("Mysql connected...");
     }
-})
+});
 
 app.get('/', (req, res) => {
-    res.send("hello");
-})
+    res.render("index");
+});
+
+app.get('/login', (req, res) => {
+    res.render("login");
+});
+
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+app.post('/login', (req, res) => {
+    console.log(req.body);
+    res.json(req.body);
+});
+
+app.post('/register', (req, res) => {
+    res.json(req.body);
+});
 
 
 app.listen(process.env.PORT, (req, res) =>  {
